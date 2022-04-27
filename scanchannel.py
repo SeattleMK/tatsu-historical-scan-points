f5279628388f14892694fa3404983da4830c65e1
Jordan Banasik <woovie@woovie.net>
2022-04-27 11:29:02 -0700

    Large rewrite of ScanChannel
    I created a test file with a fake discord.Channel class to test the
    implementation of the ScanChannel class as I could not properly figure
    out exactly how to do this.
    
    Using that knowledge I rebuilt this class from scratch. It is now working
    properly when testing in my personal Discord.

--- scanchannel.py
+++ scanchannel.py
@@ -2,9 +2,8 @@
 ScanChannel
 """
 
-from dataclasses import field
+from types import NoneType
 import discord
-from userhistory import UserHistory
 
 class ScanChannel:
     # https://discordpy.readthedocs.io/en/stable/api.html#discord.TextChannel.history
@@ -16,33 +15,41 @@ class ScanChannel:
         self,
         channel: discord.TextChannel,
         limit: int = 100,
-        user_history: UserHistory = field(default_factory=UserHistory),
+        user_history: dict = None,
     ) -> None:
         self.channel = channel
-        self.current_message = None
         self.limit = limit
         self.user_history = user_history
+        if isinstance(self.user_history, NoneType):
+            self.user_history = {}
+        self.pause = False
 
-    def start_scan(self):
-        if not self.current_message:
-            self.current_message = self.channel.last_message
-        self.scan(
-            self.current_message
-        )  # TODO replace with a method that keeps running repeatedly
+    async def start_scan(self):
+        await self.scan_channel()
 
-    def stop_scan(self):  # TODO Destroy the object instead? This may not be necessary.
-        pass
+    async def scan_channel(self):
+        print(f"starting scan of {self.channel.name}")
+        start_message = None
+        end_message = None
+        reached_end = False
+        while not reached_end:
+            if end_message:
+                start_message = end_message
+            end_message = await self.scan(start_message)
+            if end_message == start_message:
+                reached_end = True
+                print("done with scan")
 
-    def pause_scan(self):
-        pass
-
-    def scan(self, origin_message: discord.Message):
-        messages_handled = 0
-        for message in self.channel.history(origin_message, limit=self.limit):
-            user_id = message.author.id
-            if not user_id in self.user_history:
-                self.user_history[user_id] = 0
-            self.user_history[user_id] += 1
-            messages_handled += 1
-            if messages_handled == 100:
-                self.current_message = message
+    async def scan(self, start_message: discord.Message = None) -> discord.Message:
+        last_message = None
+        async for message in self.channel.history(after=start_message):
+            if message.author.bot:
+                continue
+            author_id = message.author.id
+            if not author_id in self.user_history:
+                self.user_history[author_id] = 0
+            self.user_history[author_id] += 1
+            last_message = message
+        if isinstance(last_message, NoneType):
+            last_message = start_message
+        return last_message
